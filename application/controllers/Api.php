44cf784439822b3f8afbffa1da444ff73945328c
Soumyajeet Seal <soumyajeet009@gmail.com>
2021-06-16 23:12:20 +0530

    First Commit
    This is basic files for converting normal calculator with REST Controller in Codeignitor 3

---
+++ application/controllers/Api.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require(APPPATH.'/libraries/REST_Controller.php');
+use Restserver\Libraries\REST_Controller;
+class Api extends REST_Controller
+{
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['form_validation','encryption', 'encrypt','session','javascript','image_lib','pagination']);
+        $this->load->helper(['url', 'form', 'date','admin_helper','jwt_helper']);
+        $this->encryption->create_key(16);
+        $this->load->model(['api_model']);
+        $this->gallery_path = realpath(APPPATH . '../uploads');
+    }
+}
